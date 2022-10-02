15919befcfd6b1c8d420fc943fe28ca6fa457b43
Екатерина Калистратова <kalistratova_k@mail.ru>
2022-10-02 17:05:07 +0300

    1

--- src/test/java/ru/netlogy/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netlogy/service/CashbackHackServiceTest.java
@@ -1,15 +1,22 @@
 package ru.netlogy.service;
 
 import org.junit.Test;
-
 import static org.junit.Assert.*;
 
 public class CashbackHackServiceTest {
-    @Test
+   @Test
  public void remain() {
         CashbackHackService service = new CashbackHackService();
         int amount = 800;
         int expected = 200;
+        int actual = service.remain(amount);
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void remain() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int expected = 0;
         int actual = service.remain(amount);
         assertEquals(expected, actual);
     }
